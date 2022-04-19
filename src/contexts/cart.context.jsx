34d7e74970bcc472a5374d5f5375beabd1b5206c
DoniaMahmoud <donia1999mahmoud@gmail.com>
2022-04-20 00:49:14 +0200

    7. added addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,74 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
+//CHECK IF PRODUCT ALREADY EXISTS OR NOT
+const addCartItem = (cartItems, productToAdd) => {
+  //find if cartItems contain productToAdd
+  // Same as map , find func that will exit giving the cartItem that return the true boolean
+  //based on the callback
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  //If found , increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //return new array with modified cartItems/ new cart item
+  //empty cart case (adding product for the first time)
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
+/*
+PRODUCT
+{
+  ID,
+  NAME,
+  PRICE,
+  IMAGEURL,
+}
+CART ITEM
+{
+  ID,
+  NAME,
+  PRICE,
+  IMAGEURL,
+  QUANTITY
+}
+
+*/
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  //re-count total quantity everytime cartItem changes
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
